9f5c3cbb27640327d142e6d48b131e86a2aba31e
Fawzi El Gamal <fawzielgamal@icloud.com>
2023-06-17 16:24:09 -0300

    Borrando entorno virtual subido por error

--- proyecto/Tienda_de_repuestos/App_Tienda_de_repuestos/context_processor.py
+++
@@ -1,7 +0,0 @@
-def total_price(request):
-    total = 100
-
-    if request.user.is_authenticated:
-        for key, value in request.method['cart'].items():
-            total = total + (float(value['price'])*value['quantity'])
-    return {"total_price": total}
